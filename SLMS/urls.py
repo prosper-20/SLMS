7b72f7340e6b65a32163ed9378746217f49252cd
prosper <edwardprosper001@gmail.com>
2023-10-27 20:25:15 +0100

    uploads

--- SLMS/urls.py
+++ SLMS/urls.py
@@ -28,7 +28,8 @@ urlpatterns = [
     path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('course/', include('courses.urls')),
     path('', CourseListView.as_view(), name='course_list'),
-    path("api/", include("api.urls"))
+    path("api/", include("api.urls")),
+    path('students/', include('students.urls')),
 ]
 
 if settings.DEBUG:
