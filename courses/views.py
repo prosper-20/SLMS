e75891945c1797ca4826408dff1bcfc41e4ffb42
prosper <edwardprosper001@gmail.com>
2023-10-27 20:45:05 +0100

    uploads

--- courses/views.py
+++ courses/views.py
@@ -18,34 +18,7 @@ from django.views.generic.detail import DetailView
 from students.forms import CourseEnrollForm
 
 from django.views.generic.list import ListView
-from courses.models import Course
 
-from django.views.generic.detail import DetailView
-class StudentCourseDetailView(DetailView):
-    model = Course
-    template_name = 'students/course/detail.html'
-
-
-    def get_queryset(self):
-        qs = super().get_queryset()
-        return qs.filter(students__in=[self.request.user])
-    
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        course = self.get_object()
-        if 'module_id' in self.kwargs:
-            context['module'] = course.modules.get(id=self.kwargs['module_id'])
-        else:
-            context['module'] = course.modules.all()[0]
-        return context
-
-class StudentCourseListView(LoginRequiredMixin, ListView):
-    model = Course
-    template_name = 'students/course/list.html'
-
-    def get_queryset(self):
-        qs = super().get_queryset()
-        return qs.filter(students__in=[self.request.user])
 
 class CourseDetailView(DetailView):
     model = Course
