77d5959ad477275cc795e22d56cdd6c460ca18fc
prosper <edwardprosper001@gmail.com>
2023-11-05 19:33:24 +0100

    SLMS

--- SLMS/settings.py
+++ SLMS/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['slms-8bb90aa2c295.herokuapp.com']
 
 
 # Application definition
